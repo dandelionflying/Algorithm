d20c05c1cb792db19ca644efe3fde12bacf6d9e7
zhuzexiong <zhuzexiong@vip.qq.com>
2021-05-17 17:29:08 +0800

    排序校验

--- src/main/java/cn/running4light/demo/utils/SortingUtil.java
+++ src/main/java/cn/running4light/demo/utils/SortingUtil.java
@@ -1,5 +1,6 @@
 package cn.running4light.demo.utils;
 
+import cn.running4light.demo.template.sort.InsertionSorting;
 import cn.running4light.demo.template.sort.SelectiveSorting;
 
 import java.lang.reflect.InvocationTargetException;
@@ -24,12 +25,54 @@ public class SortingUtil {
         if("SelectiveSorting".equals(sortName)){
             SelectiveSorting.sort(data, sort);
         }
+        if("InsertionSorting".equals(sortName)){
+            InsertionSorting.sort(data, sort);
+        }
+
         long endTime = System.nanoTime();
-        /*for (T d :
+        for (T d :
                 data) {
             System.err.print(d+" ");
-        }*/
+        }
         double result = (endTime - time) / 1000000000.0;
         System.err.println("数组长度:"+data.length+"\t执行时间："+result+" s");
+    }
+    /**
+     * @Description 排序校验
+     * @Author running4light朱泽雄
+     * @CreateTime 17:28 2021/5/17
+     */
+    public static <T extends Comparable<T>> boolean checkSorted(T[] data, String sort){
+        if("asc".equals(sort)){
+            return checkDesc(data);
+        }else{
+            return checkAsc(data);
+        }
+    }
+    /**
+     * @Description 递减判断
+     * @Author running4light朱泽雄
+     * @CreateTime 17:27 2021/5/17
+     */
+    public static <T extends Comparable<T>> boolean checkDesc(T[] data){
+        for (int i = 1; i < data.length; i++) {
+            // 若非递减
+            if(data[i - 1].compareTo(data[i]) < 0)
+                return false;
+        }
+        return true;
+    }
+    /**
+     * @Description 递增判断
+     * @Author running4light朱泽雄
+     * @CreateTime 17:27 2021/5/17
+     */
+    public static <T extends Comparable<T>> boolean checkAsc(T[] data){
+        for (int i = 1; i < data.length; i++) {
+            // 若非递增
+            if(data[i - 1].compareTo(data[i]) > 0)
+                return false;
+        }
+        return true;
     }
 }
