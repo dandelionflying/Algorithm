14b7a4b2ff868cd297e2bdaa9203b2a983761f4f
zhuzexiong <zhuzexiong@vip.qq.com>
2021-05-21 10:45:03 +0800

    leetcode--移除链表元素

---
+++ src/main/java/cn/running4light/demo/utils/ListNode.java
@@ -0,0 +1,26 @@
+package cn.running4light.demo.utils;
+
+/**
+ * @author running4light
+ * @description
+ * @createTime 2021/5/21 10:42
+ */
+public class ListNode {
+
+
+    public int val;
+    public ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+}
